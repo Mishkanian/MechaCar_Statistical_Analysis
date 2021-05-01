b37407dd97f89d89b3adecb84a70c52e90b4d69a
Michael Mishkanian <77469811+Mishkanian@users.noreply.github.com>
2021-05-01 14:31:04 -0700

    Uploading deliverable 2 and images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,5 @@
 # Deliverable 1 - Michael Mishkanian
+library(tidyverse)
 library(dplyr)
 # import and read mechacar csv as a dataframe
 MechaCar_mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
@@ -6,3 +7,11 @@ MechaCar_mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFa
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar_mpg_table)
 # determine p-value and r^2
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar_mpg_table))
+
+# Deliverable 2 - Michael Mishkanian
+# import and read suspension_coil csv as a dataframe
+Suspension_Coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+# create total summary dataframe
+total_summary <- Suspension_Coil_table %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), STDEV_PSI=sd(PSI))
+# create lot summary dataframe
+lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), STDEV_PSI=sd(PSI), .groups = 'keep')
